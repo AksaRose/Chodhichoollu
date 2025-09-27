19eef94939c472d33b3383757df6304125c29911
Aksa Rose <117498997+AksaRose@users.noreply.github.com>
2025-09-27 15:24:15 +0530

    build:changed the embedding model to lighter one

--- store.py
+++ store.py
@@ -2,5 +2,5 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 vector_store = InMemoryVectorStore(embeddings)
